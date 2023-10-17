c15e13a3f8f9465e874ddd946be450cfe11ea080
narmina <namira.lir@gmail.com>
2023-10-17 23:33:13 +0300

    End

--- content_load.py
+++ content_load.py
@@ -4,7 +4,7 @@ import os.path
 
 def get_content():
     # получает массив из файла
-    with open(os.path.join('data', 'content.json'), 'r') as file:
+    with open(os.path.join('data', 'content.json'), 'r', encoding='utf-8') as file:
         content = file.read()
         content_json = json.loads(content)
         return content_json
